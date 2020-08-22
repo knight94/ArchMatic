7f04005da5707447008f7af027ef84347a7bb85b
Naman Jain <NJ@xyz.com>
2020-08-22 17:21:41 +0530

    0-Pacman.sh: Add script for initializing pacman

---
+++ 0-Pacman.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#      _          _    __  __      _   _    
+#     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__ 
+#    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
+#   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__| 
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+echo
+echo "SETTING UP PACMAN AND ROOT UTILITY"
+echo
+
+pacman -Sy
+pacman-key --init
+pacman-key --populate archlinuxarm
+
+PKGS=(
+        'sudo'       # utility for root control
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo
+echo "Done!"
+echo
